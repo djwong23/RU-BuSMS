6c0f158b9588d8c4e7618ff58a135b962b3ff936
RahulNTrivedi <55066036+RahulNTrivedi@users.noreply.github.com>
2019-10-20 00:44:02 -0400

    Add files via upload
    Split Busch, added Times

--- emojipasta.py
+++ emojipasta.py
@@ -6,6 +6,7 @@ import googlemaps
 import os
 import requests, json 
 from shapely.geometry import Point, Polygon
+import datetime
 
 
 app = Flask(__name__)
@@ -24,23 +25,53 @@ def sms_reply():
                     
     directions = location_finder(starting) + " to " + location_finder(end)
     print(directions)
-    
+
+    currentDT = datetime.datetime.now()
     
 
-    if "livi to busch" in directions.lower() or "busch to livi" in directions.lower():
-        resp.message("Take B!")
+    if "livi to buschh" in directions.lower() or "buschh to livi" in directions.lower() or "buscha to livi" in directions.lower() or "livi to buscha" in directions.lower():
+        if currentDT.hour > 6 or currentDT.hour < 2 or (currentDT.hour == 2 and currentDT.minute <20):
+            resp.message("Take B!")
+        else :
+            resp.message("No Bus Route!")
     elif "college ave to livi" in directions.lower() or "livi to college ave" in directions.lower():
-        resp.message("Take LX!")
-    elif "busch to college ave" in directions.lower() or "busch to college ave" in directions.lower():
-        resp.message("Take A or H!")
+        if currentDT.hour > 6 or currentDT.hour < 2 or (currentDT.hour == 2 and currentDT.minute <15) :
+            resp.message("Take LX!")
+        else :
+            resp.message("No Bus Route!")
+    elif "buscha to college ave" in directions.lower() or "college ave to buscha" in directions.lower() or "buschh to college ave" in directions.lower() or "college ave to buschh" in directions.lower():
+        if currentDT.hour > 7 and currentDT.hour < 21 :
+            if "buscha to college ave" in directions.lower() or "college ave to buscha" in directions.lower():
+                resp.message("Take A!")
+            elif "buschh to college ave" in directions.lower() or "college ave to buschh" in directions.lower():
+                resp.message("Take H!")            
+        elif currentDT.hour > 6 or currentDT.hour < 2 or (currentDT.hour ==2 and currentDT.minute <30) :
+            resp.message("Take H")
+        else :
+            resp.message("No Bus Route!")
     elif "college ave to cook" in directions.lower() or "cook to college ave" in directions.lower() or "college ave to douglass" in directions.lower() or "douglass to college ave" in directions.lower():
-        resp.message("Take EE or F!")
+        if currentDT.hour > 7 and currentDT.hour < 21 :
+            resp.message("Take EE or F!")
+        elif currentDT.hour > 6 or currentDT.hour < 2 or (currentDT.hour == 2 and currentDT.minute < 15) :
+            resp.message("Take EE!")
+        else :
+            resp.message("No Bus Route!")
     elif "livi to cook" in directions.lower() or "cook to livi" in directions.lower() or "livi to douglass" in directions.lower() or "douglass to livi" in directions.lower():
-        resp.message("Take REXL!")
-    elif "busch to cook" in directions.lower() or "cook to busch" in directions.lower():
-        resp.message("Take REXB!")
+        if currentDT.hour > 7 and currentDT.hour < 23 :
+            resp.message("Take REXL!")
+        elif currentDT.hour > 6 or currentDT.hour < 2 or (currentDT.hour == 2 and currentDT.minute <15):
+            resp.message("Take an LX and then an EE!")
+        else :
+            resp.message("No Bus Route!")
+    elif "buscha to cook" in directions.lower() or "cook to buscha" in directions.lower() or "cook to buschh" in directions.lower() or "buschh to cook" in directions.lower():
+        if currentDT.hour > 7 and currentDT.hour < 23 :
+            resp.message("Take REXB!")
+        elif currentDT.hour > 6 or currentDT.hour < 2 or (currentDT.hour == 2 and currentDT.minute <15):
+            resp.message("Take an H and then an EE!")
+        else :
+            resp.message("No Bus Route!")
     else:
-        resp.message("Error")
+        resp.message("No Bus Route!")
     return str(resp)
 
 def location_finder(a_string) :
@@ -74,15 +105,18 @@ def location_finder(a_string) :
     polyli = Polygon([(40.524985, -74.448393), (40.505837, -74.440712), (40.513665, -74.425476), (40.525278, -74.427106), (40.532097, -74.442729)]) 
     polycd = Polygon([(40.490120, -74.441927), (40.467900, -74.455505), (40.457437, -74.427237), (40.474610, -74.415324), (40.491094, -74.435263)])
     polybu = Polygon([(40.510755, -74.462606), (40.522043, -74.488226), (40.531242, -74.467326), (40.526509, -74.447241), (40.516821, -74.453554)]) 
-
+    polybuA = Polygon([(40.510755, -74.462606), (40.521020, -74.460552),(40.529854, -74.461346), (40.526509, -74.447241), (40.516821, -74.453554)])
+    polybuH = Polygon([(40.510755, -74.462606), (40.522043, -74.488226), (40.531242, -74.467326), (40.529854, -74.461346), (40.521020, -74.460552)])
     if polyca.contains(pt) :
         campus = 'college ave'
     elif polyli.contains(pt) :
         campus = 'livi'
     elif polycd.contains(pt) :
         campus = 'cook'
-    elif polybu.contains(pt) :
-        campus = 'busch'
+    elif polybuA.contains(pt) :
+        campus = 'buscha'
+    elif polybuH.contains(pt) :
+        campus = 'buschh'
     else :
         campus = 'livi'
     return campus
